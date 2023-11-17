5d3ada2ada6dbdf0ea55e11dbb7caa14fc5e5e7c
Michail Mengidis <megidismix@gmail.com>
2023-11-17 17:43:08 +0200

    fixes some lines of code that had errors/junk

---
+++ app/src/main/java/gr/ihu/tourguide/View/SignUpActivity.java
@@ -0,0 +1,16 @@
+package gr.ihu.tourguide.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import gr.ihu.tourguide.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
