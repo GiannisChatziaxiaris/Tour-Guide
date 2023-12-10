6c499af2612dc30ac06b284702032134d1b84ac0
BIBOS <popularmmos2014@gmail.com>
2023-12-10 21:30:20 +0200

    Google maps leitourgiko /Todo problhma me google places api +allages sta onomata

--- app/src/main/java/gr/ihu/tourguide/MapMainActivity.java
+++ app/src/main/java/gr/ihu/tourguide/MapMainActivity.java
@@ -4,27 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import com.google.android.gms.location.places.Places;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
 import gr.ihu.tourguide.View.MapActivity;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+public class MapMainActivity extends AppCompatActivity {
+    private static final String TAG = "MapMainActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_map);
 
         if(isServisesOK()){
             init();
@@ -36,14 +34,14 @@ public class MainActivity extends AppCompatActivity {
         btnMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                Intent intent = new Intent(MapMainActivity.this, MapActivity.class);
                 startActivity(intent); // Add this line to start MapActivity
             }
         });
     }
     public boolean isServisesOK() {
         Log.d(TAG, "isServicesOk: checking google services versuon:");
-        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapMainActivity.this);
         if (available == ConnectionResult.SUCCESS) {
             //everything is fine and the user can make map requests
             Log.d(TAG, "isServicesOK: Google Play Services is working");
@@ -51,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
             //an error occured but we can resolved it
             Log.d(TAG, "isServicesOk: an error occured but we can fix it");
-            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MapMainActivity.this, available, ERROR_DIALOG_REQUEST);
             dialog.show();
         }else{
             Toast.makeText(this, "YOu cant make map requests", Toast.LENGTH_SHORT).show();
