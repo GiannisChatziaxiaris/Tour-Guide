89d886523ba4f663bce5625445a2b8f12f8aeee7
GiannisChatziaxiaris <gxatziaxiaris@gmail.com>
2024-01-15 00:26:42 +0200

    removed unused classes and packets

--- app/src/main/java/gr/ihu/tourguide/interfaces/IntUserService.java
+++
@@ -1,7 +0,0 @@
-package gr.ihu.tourguide.interfaces;
-
-import gr.ihu.tourguide.model.TourGuideModel;
-
-public interface IntUserService {
-    boolean createUser(TourGuideModel model, String username, String password);
-}
